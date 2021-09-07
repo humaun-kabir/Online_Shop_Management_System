83a607ea8fd2e0f116642953444ddd8cffb80343
humaun-kabir <asmhumaunkabir@gmail.com>
2021-09-07 15:12:04 +0600

    load single product complete

--- preview.php
+++ preview.php
@@ -1,19 +1,48 @@
 <?php include 'inc/header.php'; ?>
 
+<?php
+    if(!isset($_GET['proid']) || $_GET['proid'] == NULL){ // Get this id from catadd.php and take this on $id variable.
+        echo "<script>window.location = '404.php'; </script>"; // we transfer to catlist.php page
+    }else {
+        $id = $_GET['proid']; // Get this id from catadd.php and take this on $id variable.
+    }
+?>
+
+
+<?php
+    
+    /*
+    if($_SERVER['REQUEST_METHOD'] == 'POST'){
+        $brandName = $_POST['brandName']; // here i add our input filed name 
+
+        $updateBrand = $brand->brandUpdate($brandName,$id); // with this Category object i access one method. 
+    }
+	*/
+
+
+?>
+
  <div class="main">
     <div class="content">
     	<div class="section group">
-				<div class="cont-desc span_1_of_2">				
+				<div class="cont-desc span_1_of_2">	
+					
+				<?php
+				$getPd = $pd->getSingleProduct($id);
+				if($getPd){
+					while($result = $getPd->fetch_assoc()){					
+
+				?>
 					<div class="grid images_3_of_2">
-						<img src="images/preview-img.jpg" alt="" />
+						<img src="admin/<?php echo $result['image']; ?>" alt="" />
 					</div>
 				<div class="desc span_3_of_2">
-					<h2>Lorem Ipsum is simply dummy text </h2>
-					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore.</p>					
+					<h2><?php echo $result['productName']; ?></h2>
+					<p><?php echo $fm->textShorten($result['body'], 200); ?></p>					
 					<div class="price">
-						<p>Price: <span>$500</span></p>
-						<p>Category: <span>Laptop</span></p>
-						<p>Brand:<span>Samsnumg</span></p>
+						<p>Price: <span>$<?php echo $result['price']; ?></span></p>
+						<p>Category: <span><?php echo $result['catName']; ?></span></p>
+						<p>Brand:<span><?php echo $result['brandName']; ?></span></p>
 					</div>
 				<div class="add-cart">
 					<form action="cart.html" method="post">
@@ -24,9 +53,10 @@
 			</div>
 			<div class="product-desc">
 			<h2>Product Details</h2>
-			<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
-	        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+			<?php echo $result['body']; ?>
 	    </div>
+
+		<?php } } ?>
 				
 	</div>
 				<div class="rightsidebar span_3_of_1">
