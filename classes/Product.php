3a6e8b446de5202174cb6537868aab832ea9aeba
humaun-kabir <asmhumaunkabir@gmail.com>
2021-09-07 14:05:27 +0600

    show latest product in home page

--- classes/Product.php
+++ classes/Product.php
@@ -188,6 +188,12 @@
         $result = $this->db->select($query);
         return $result;
     }
+
+    public function getNewProduct(){
+        $query = "SELECT * FROM tbl_product ORDER BY productId DESC LIMIT 4";
+        $result = $this->db->select($query);
+        return $result;
+    }
     
 }
 
