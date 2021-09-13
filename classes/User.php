0595f8729ae8a2853e7f9fc186d2a6d44dbf3633
humaun-kabir <asmhumaunkabir@gmail.com>
2021-09-13 15:39:53 +0600

    edit customer profile

--- classes/User.php
+++ classes/User.php
@@ -80,6 +80,44 @@
             $result = $this->db->select($query);
             return $result;
         }
+
+        public function customerUpdate($data, $cmrId){
+            $name    = mysqli_real_escape_string($this->db->link, $data['name']);
+            $address = mysqli_real_escape_string($this->db->link, $data['address']);
+            $city    = mysqli_real_escape_string($this->db->link, $data['city']);
+            $country = mysqli_real_escape_string($this->db->link, $data['country']);
+            $zip     = mysqli_real_escape_string($this->db->link, $data['zip']);
+            $phone   = mysqli_real_escape_string($this->db->link, $data['phone']);
+            $email   = mysqli_real_escape_string($this->db->link, $data['email']);
+        
+            if($name == '' || $address == '' || $city == '' || $country == '' || $zip == '' || $phone == ''
+             || $email == '' ){
+                $msg = "<span class='error'>Field must not be empty.</span>";
+                    return $msg;
+            } else{
+        
+                $query = "UPDATE tbl_customer
+                SET 
+                name    = '$name',
+                address = '$address',
+                city    = '$city',
+                country = '$country',
+                zip     = '$zip',
+                phone   = '$phone',
+                email   = '$email'
+                WHERE id = '$cmrId' ";
+
+                $update_row = $this->db->update($query);
+                if($update_row){
+                    $msg = "<span class='success'>Customer Data Updated Succesfully.</span>";
+                    return $msg;
+                }else{
+                    $msg = "<span class='error'>Customer Data Not Updated.</span>";
+                    return $msg;
+                } 
+                    }
+            
+            }
     }
 
 ?>
--- profile.php
+++ profile.php
@@ -26,6 +26,10 @@
 
         <table class="tblone">
             <tr>
+                <td colspan="3"><h2>Your Profile Details</h2></td>
+            </tr>
+
+            <tr>
                 <td width="20%">Name</td>
                 <td width="5%"> : </td>
                 <td><?php echo $result['name']; ?></td>
@@ -65,6 +69,12 @@
                 <td>Country</td>
                 <td> : </td>
                 <td><?php echo $result['country']; ?></td>
+            </tr>
+
+            <tr>
+                <td></td>
+                <td></td>
+                <td><a href="editprofile.php">Update Details</a></td>
             </tr>
         </table>
 
